287f2bc634b2e53e18a6f0b98fe153c45adfc209
Anton Malygin <toxaval@gmail.com>
2023-12-10 17:50:45 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "LibXray",
-            url: "LibXray.xcframework.zip",
+            url: "https://github.com/aimalygin/LibXray/releases/download/2.0.0/LibXray.xcframework.zip",
             checksum: "d4284dc104823cbd65ef7c4cbe1f038868a881e7ba81caed9eefc06a3a25668c"
         )
     ]
