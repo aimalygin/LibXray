fcebe027a84022118ad745e1021776d5a5810636
Anton Malygin <ai.malygin@icloud.com>
2026-01-22 11:25:40 -0600

    Update libXray to v25.12.8

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "LibXray",
-            url: "https://github.com/aimalygin/LibXray/releases/download/v25.8.3/libxray-v25.8.3.xcframework.zip",
-            checksum: "c2d024856615c4f49690d663acf8b82e2c1565a8da504ebe56091396585a0009"
+            url: "https://github.com/aimalygin/LibXray/releases/download/v25.12.8/libxray-v25.12.8.xcframework.zip",
+            checksum: "96666e257bd3a130694e71c3051cd4c9a00bf77ce62b535fae32bbdd8511d93b"
         )
     ]
 )
